ff6004fa44ef3c57c665930f6563a20439eeea5d
Frankie <fdsimms@gmail.com>
2016-05-20 13:38:31 -0400

    Begin creating groceryList.js.

---
+++ js/groceryList.js
@@ -0,0 +1,213 @@
+export default [
+  {
+    "id": 1,
+    "name": "Produce",
+    "sections": [
+      {
+        "id":2,
+        "name": "Fruit",
+        "sections": [
+          {
+            "id":3,
+            "name": "Apples",
+            "sections": [
+              {
+                "id":4,
+                "name": "Delicious",
+                "sections": [
+                  {
+                    "id":5,
+                    "name": "Golden Delicious",
+                    "sections": [
+
+                    ]
+                  },
+                  {
+                    "id":6,
+                    "name": "Red Delicious",
+                    "sections": [
+
+                    ]
+                  }
+                ]
+              },
+              {
+                "id":7,
+                "name": "Macintosh",
+                "sections": []
+              }
+            ]
+          },
+          {
+            "id":8,
+            "name": "Bananas",
+            "sections": [
+              {
+                "id":9,
+                "name": "Cavendish",
+                "sections": []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "id":10,
+        "name": "Veggies",
+        "sections": [
+          {
+            "id":11,
+            "name": "Greens",
+            "sections": [
+              {
+                "id":12,
+                "name": "Arugula",
+                "sections": []
+              },
+              {
+                "id":13,
+                "name": "Lettuce",
+                "sections": [
+                  {
+                    "id":14,
+                    "name": "Iceberg",
+                    "sections": []
+                  },
+                  {
+                    "id":15,
+                    "name": "Romaine",
+                    "sections": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "id":16,
+            "name": "Potatoes",
+            "sections": [
+              {
+                "id":17,
+                "name": "Russet",
+                "sections": []
+              },
+              {
+                "id":18,
+                "name": "Fingerling",
+                "sections": []
+              }
+            ]
+          },
+          {
+            "id":19,
+            "name": "Rutabagas",
+            "sections": []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 20,
+    "name": "Dairy",
+    "sections": [
+      {
+        "id":21,
+        "name": "Cheeses",
+        "sections": [
+          {
+            "id":23,
+            "name": "Moldy",
+            "sections": [
+              {
+                "id":24,
+                "name": "Blue",
+                "sections": [
+                  {
+                    "id":25,
+                    "name": "Stilton",
+                    "sections": []
+                  },
+                  {
+                    "id":26,
+                    "name": "Gorgonzola",
+                    "sections": []
+                  }
+                ]
+              },
+              {
+                "id":27,
+                "name": "Camembert",
+                "sections": []
+              }
+            ]
+          },
+          {
+            "id":28,
+            "name": "Hard",
+            "sections": [
+              {
+                "id":29,
+                "name": "Cheddar",
+                "sections": []
+              },
+              {
+                "id":30,
+                "name": "Parmesan",
+                "sections": []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "id":31,
+        "name": "Milk",
+        "sections": [
+          {
+            "id":32,
+            "name": "Milk Alternatives",
+            "sections": [
+              {
+                "id":33,
+                "name": "Soy Milk",
+                "sections": []
+              },
+              {
+                "id":34,
+                "name": "Almond Milk",
+                "sections": []
+              },
+              {
+                "id":35,
+                "name": "Coconut Milk",
+                "sections": []
+              }
+            ]
+          },
+          {
+            "id":36,
+            "name": "Cow's Milk",
+            "sections": [
+              {
+                "id":37,
+                "name": "1%",
+                "sections": []
+              },
+              {
+                "id":38,
+                "name": "Whole",
+                "sections": []
+              }
+            ]
+          },
+          {
+            "id":39,
+            "name": "Butter",
+            "sections": []
+          }
+        ]
+      }
+    ]
+  }
+];
