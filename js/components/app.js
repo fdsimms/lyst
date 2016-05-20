ae3732d1e2a1084a5573138afb722ab39b439b98
Frankie <fdsimms@gmail.com>
2016-05-20 01:29:35 -0400

    Some more styling.

--- js/components/app.js
+++ js/components/app.js
@@ -5,13 +5,15 @@ import List from "./list";
 const App = () => (
       <div className="app">
         <header className="header"><h1>Lyst</h1></header>
-        <ul className="list-container">
-          {data.map(list =>
-            list.sections.length > 0 ?
-            <List key={list.id} list={list} /> :
-            <li key={list.id}>{list.name}</li>
-          )}
-        </ul>
+        <main>
+          <ul className="list-container">
+            {data.map(list =>
+              list.sections.length > 0 ?
+              <List key={list.id} list={list} /> :
+              <li key={list.id}>{list.name}</li>
+            )}
+          </ul>
+        </main>
       </div>
     );
 
